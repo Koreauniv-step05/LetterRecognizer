b8404a04487e64e7b08012f9fc02bfc51c40fbc2
pjo901018 <pjo901018@korea.ac.kr>
2017-02-25 02:47:19 +0900

    Predictor class로 변환, init부분에서 saver한번만 불러오게 수정

--- recog_src/recog_runner.py
+++ recog_src/recog_runner.py
@@ -6,27 +6,31 @@ from tensorflow.examples.tutorials.mnist import input_data
 from recog_runtoolbox import fill_feed_dict, do_eval
 from recog_model import placeholder_inputs, graph_model, calcul_loss, training, evaluation
 
-def run_recognize(input, HYPARMS):
-    placebundle = placeholder_inputs(1)
-    logits = graph_model(placebundle)
-    sftmax = tf.nn.softmax(logits)
-    classified = tf.argmax(sftmax,1)
-    saver = tf.train.Saver()
 
-    with tf.Session() as sess:
+class Predictor:
+    def __init__(self,HYPARMS):
+        self.init(HYPARMS)
+
+    def predict(self, input):
+        with self.sess.as_default():
+            return self.classified.eval(feed_dict = {self.placebundle.x: input,
+                                                    self.placebundle.keep_prob: 1})
+
+    def init(self,HYPARMS):
+        self.sess = tf.Session()
+        self.placebundle = placeholder_inputs(1)
+
+        self.init = tf.initialize_all_variables()
+        self.saver = tf.train.Saver()
+        self.sess.run(self.init)
+
+        self.logits = graph_model(self.placebundle)
+        self.sftmax = tf.nn.softmax(self.logits)
+        self.classified= tf.argmax(self.sftmax,1)
 
         ckpt = tf.train.get_checkpoint_state(HYPARMS.ckpt_dir)
         if ckpt and ckpt.model_checkpoint_path:
-            # Restores from checkpoint
-            saver.restore(sess, ckpt.model_checkpoint_path)
+            self.saver.restore(self.sess, ckpt.model_checkpoint_path)
             print "Model loaded"
         else:
             print "No checkpoint file found"
-
-        feed_dict = fill_feed_dict(input,
-                                   placebundle.x,
-                                   placebundle.y_,
-                                   placebundle.keep_prob,
-                                   HYPARMS)
-
-        return classified.eval(feed_dict)
--- recognizer.py
+++ recognizer.py
@@ -1,5 +1,5 @@
 from recog_src.recog_class import Hyparms
-from recog_src.recog_runner import run_recognize
+from recog_src.recog_runner import Predictor
 from recog_src.recog_loadimage import load_allpath, load_image
 from recog_src.recog_preproc import preprocess
 import tensorflow as tf
@@ -15,12 +15,14 @@ def main(_):
         tf.gfile.DeleteRecursively(HYPARMS.log_dir)
     tf.gfile.MakeDirs(HYPARMS.log_dir)
 
+    predictor = Predictor(HYPARMS)
+
     paths = load_allpath(HYPARMS.recog_data_dir)
     for path in paths:
         image = load_image(path)
         image = preprocess(image)
         print(path)
-        print(run_recognize(image,HYPARMS))
+        print(predictor.predict(image))
 
 if __name__ == '__main__':
   tf.app.run(main=main)
