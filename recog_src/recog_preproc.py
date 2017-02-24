bad8333bd54f6084190697cd95603f25357c8974
pjo901018 <pjo901018@korea.ac.kr>
2017-02-25 01:20:55 +0900

    input data float으로 바꿔읽기

--- recog_src/recog_preproc.py
+++ recog_src/recog_preproc.py
@@ -4,9 +4,9 @@ def preprocess(input):
     input = input.resize([28,28])
     input = input.convert('L')
     input = convert2np(input)
-    input = input/255
+    input = input.astype(float)/255
     print(input)
-    input = input.reshape([1, 784])
+    dinput = input.reshape([1, 784])
     return input
 
 def convert2np(input):
